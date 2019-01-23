cb59e2f72aaa87c336b8da41415dd8d92af62645
nick <goddl717@naver.com>
2019-01-23 11:30:56 +0900

    vue 생애주기를 보자

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,44 @@
 <!--html  vue 템플릿 -->
 <template>
-    <div>안녕하세요!!</div>
-
+    <div>{{greeting}}{{count}}</div>
+   
 </template>
 
-<!-- JS  vue 인스턴스 -->
+<!-- JS  vue 인스턴스. -->
+<!-- 변수를 선언 함수를 선언.-->
 <script>
-    //
-    export default{} 
+    //변수 선언 방법//특이한 방법인거같다.
+
+    export default{
+        //변수 선언 방법
+        data : function(){
+            return {
+                greeting: '안녕하세요',
+                count: 0,
+            }
+        },
+        //함수 선언 방법 
+        methods :{
+            addCount : function(){
+                this.count += 1
+            },
+        },
+        //가상으로 생성되었을때!
+        created(){
+            console.log('created!')
+        },
+        //가상으로 생성된 컴포넌트가 실제로 삽입 되었을때.
+        mounted(){
+            consol.log('mounted!')
+        },
+
+        //vue 인스턴스 라이프사이클 
+
+
+
+
+
+    } 
 </script>
 
 <!-- CSS -->
--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// vue 를 연결하자.
+
 new Vue({
     el:'#app',
     render : h => h(App),
-    
 })
-
