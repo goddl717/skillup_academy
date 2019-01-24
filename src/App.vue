3d4e608baeb866cc4102199ba3a2f329de55056a
nick <goddl717@naver.com>
2019-01-24 17:39:26 +0900

    ㄴㄹ

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,34 @@
 <!-- -->
             
 <template>
-<div>
+<div class = "sheet">
    
     <div class = "top">
-        <div class = "top-title">반 도시락 서비스</div>
+        <div class = "top-title">"MOM"BOX</div>
         <div class = "top-menu">메뉴</div>  
     </div>
 
     <div class = "middle" >
-            <div class = "right-window">리스트</div>
+            <div class = "right-window">
+                <div>리스트</div>
+                <div class = "food-line1">
+                    음식1:가격
+                    <input class = "food1-button" type = "button">
+                    음식2:가격
+                    <input class = "food2-button" type = "button">
+                    음식3:가격
+                    <input class = "food3-button" type = "button">
+                    </div>
+                    <div class = "food-line2">
+                    음식4:가격
+                    <input class = "food4-button" type = "button">
+                    음식5:가격
+                    <input class = "food5-button" type = "button">
+                    음식6:가격
+                    <input class = "food6-button" type = "button">
+                    </div>
+                 </div>
+
             
             <div class = "left-window">
                <div class = "platform">로그인
@@ -23,7 +42,7 @@
 
                      <div class = "password-box">
                         <div>password</div>
-                        <input class = "login-password" type="text"/>
+                        <input class = "login-password" type="password"/>
                     </div>
 
                     <div class = "check-box">
@@ -74,23 +93,60 @@
 
 <!-- CSS -->
 <style lang = "scss">
-.top{
-    border : 2px dotted gray;
+.sheet{
+    background-color: rgb(255,247,225);
 
+.top{
+
+    border :1.5px solid rgb(190,192,194);
+    .top-menu{
+        border : 1px solid rgb(190,192,194);
+    }
+    .top-title{
+
+        text-align : center;
+        font-size: 50px;
+    }
 }
 
 .middle{
     display : inline-block;
-    border : 2px dotted gray;
+    border : 1px solid rgb(190,192,194);
     display: flex;
 
     .right-window{
-        border : 2px dotted gray;
+        border :1px solid rgb(190,192,194);
         flex : 3;
+        .food1-button{
+            background: url("/images/sow.PNG")no-repeat;
+            border: none;
+            border-radius: 12px;
+            width: 100px;
+            height: 100px;
+            
+        }
+        .food2-button{
+            margin-right: 400px;
+            margin-bottom: 200px
+        }
+        .food3-button{
+            margin-bottom: 200px;
+        }
+        .food4-button{
+            margin-right: 400px;
+            margin-bottom: 200px
+        }
+        .food5-button{
+            margin-right: 400px;
+            margin-bottom: 200px
+        }
+        .food6-button{
+            margin-bottom:200px;
+        }     
     }
   
    .left-window{
-         border : 2px dotted gray;
+         border : 1px solid rgb(190,192,194);
         .platform{
            .id-box{
                display:flex;
@@ -102,7 +158,6 @@
    }
 
   
-   
+   }
 }
- 
 </style>
